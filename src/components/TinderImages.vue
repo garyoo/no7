b258fe76ffaa918d3a033731663c431949ada743
Gary Yoo <garyoo@naver.com>
2019-03-20 23:37:10 +0900

    IMAGE

--- src/components/TinderImages.vue
+++ src/components/TinderImages.vue
@@ -2,17 +2,29 @@
   <div>
     <div class="thumb-nail-wrapper" v-for="(item, idx) in items" v-bind:key="idx" v-on:mouseover="hover(item)" v-on:click="popModal(item)">
       <div class="thumb-nail-name">
-        <span style="font-weight: bold;">{{item.name}}, {{item.age|| '?'}}</span>
+        <span style="font-weight: bold;">{{item.name}}, {{item.age|| '?'}} 사진 {{item.info.length}}장</span>
       </div>
-      <div class="thumb-nail" v-bind:style="{ 'background-image': 'url('+ item.url +')'}">
+      <div v-if="item.info[0].video === true">
+        <video width="100%" autoplay loop controls>
+          <source v-bind:src="item.info[0].url" type="video/mp4">
+          {{item.desc}}
+        </video>
+      </div>
+      <div v-else class="thumb-nail" v-bind:style="{ 'background-image': 'url('+ item.info[0].url +')'}">
       </div>
     </div>
-    <div v-if="showModal" class="modal" v-on:click="closeModal">
+    <div v-if="showModal" class="modal">
+      <button  v-on:click="closeModal($event)" class="close">X</button>
       <div v-if="currentItem" style="width:100%;height:100%;">
-        <div v-for="(item, idx) in currentItem.existsValue" v-bind:key="idx" class="modal-thumb">
-          <div class="thumb-nail" v-bind:style="{ 'background-image': 'url('+ item.url +')'}">
-          </div>
-          <div style="background-color:rgba(255,255,255, 0.5);height:30%;font-size:1.2rem;font-weight:bold;padding: 2rem;">{{item.desc ? item.desc : 'No Description'}}</div>
+        <div style="position:absolute;top:50%;left:50px;height:100%;">
+          <a href="#prev" v-if="currentItemIndex>0" class="image-nav prev" v-on:click="imageNav(-1)">이전</a>
+        </div>
+        <div class="modal-thumb" v-if="currentItem.info[currentItemIndex]">
+          <div class="thumb-nail" v-bind:style="{ 'background-image': 'url('+ currentItem.info[currentItemIndex].url +')'}"></div>
+          <div style="background-color:rgba(255,255,255, 0.5);height:30%;font-size:1.2rem;font-weight:bold;padding: 2rem;">{{currentItem.info[currentItemIndex].desc ? currentItem.info[currentItemIndex].desc : 'No Description'}}</div>
+        </div>
+        <div style="position:absolute;top:50%;right:50px;height:100%;">
+          <a href="#next" v-if="currentItemIndex<currentItem.info.length-1" class="image-nav next" v-on:click="imageNav(1)">이후</a>
         </div>
       </div>
     </div>
@@ -24,73 +36,115 @@ import { Vue, Component, Watch } from 'vue-property-decorator'
 import 'es6-promise/auto'
 import http from 'axios'
 
-interface TinderObject {
+interface TinderInfo {
   _id: string,
   url: string,
-  age: number,
+  protocol: string,
+  host: string,
   dt: number,
-  name: string,
-  desc: string,
-  existsValue: Array<TinderObject>
+  profile: string,
+  key: string,
+  fileName: string,
+  video: boolean
 }
+
+interface TinderProfile {
+  _id: string,
+  name: string,
+  age: number,
+  profile: string,
+  desc: string,
+  dt: number,
+  info: Array<TinderInfo>
+}
+
 @Component
 export default class TinderImages extends Vue {
-  name: string = 'TinderImages'
-  items: Array<TinderObject> = []
-  currentItem : TinderObject | null = null
-  showModal: boolean = false
+  name: string = 'TinderImages';
+  items: Array<TinderProfile> = [];
+  currentItem : TinderProfile | null = null;
+  currentItemIndex: number = 0;
+  showModal: boolean = false;
 
-  hover (item: TinderObject): void {
-    console.log(item.existsValue)
-    if (item.existsValue) {
-      item.url = item.existsValue[0].url
-    }
+  imageNav (index: number) {
+    this.currentItemIndex += index
+  }
+  hover (item: TinderProfile): void {
+
   }
   created (): void {
     let result = http.post('https://api2.surveypp.com/api/getTinderImage')
     result.then(res => {
       if (res.status === 200 && res.data) {
         if (res.data['images']) {
-          let data: Array<TinderObject> = res.data['images']
+          let data: Array<TinderProfile> = res.data['images']
           for (let item of data) {
-            let image = new Image()
-            image.src = item.url
-            image.onload = () => {
-              this.items.push(item)
-            }
-            image.onerror = () => {
+            let url = item.info[0] ? item.info[0].url : ''
+            if (item.info[0].video) {
+              let req = new Request(url, {method: 'HEAD', mode: 'no-cors'})
+              fetch(req).then(res => {
+                console.log(res)
+              }).then(res => {
+                this.items.push(item)
+              }).catch(err => {
+                console.log(err)
+              })
+            } else {
+              let image = new Image()
+              image.src = url
+              image.onload = () => {
+                this.items.push(item)
+              }
+              image.onerror = () => {
+              }
             }
           }
         }
       }
     })
   }
-  popModal (item: TinderObject): void {
+  popModal (item: TinderProfile): void {
     this.currentItem = item
     this.showModal = !this.showModal
     document.body.style.overflowY = this.showModal ? 'hidden' : 'auto'
   }
-  closeModal (): void {
+  closeModal (evt: Event): void {
+    this.currentItemIndex = 0
     this.currentItem = null
     this.showModal = !this.showModal
     document.body.style.overflowY = this.showModal ? 'hidden' : 'auto'
   }
 
   @Watch('showModal')
-  modalTF(val: boolean) {
+  modalTF (val: boolean) {
 
   }
 
   @Watch('currentItem')
-  onChanged(val: TinderObject) {
+  onChanged (val: TinderProfile) {
     console.log(val)
   }
-
-
 }
 </script>
 
 <style scoped>
+  button.close {
+    background-color: transparent;
+    border: 0px;
+    float: right;
+    margin: 0px 5px 0px 0px;
+    font-size: 3.5rem;
+    color: #fff;
+    cursor: pointer;
+  }
+  button.close:hover {
+    color: brown;
+  }
+
+  a.image-nav.prev, a.image-nav.next{
+    color: #fff;
+    text-decoration: none;
+  }
   div.thumb-nail-name {
     text-align:right;
     position: absolute;
